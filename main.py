3bbb40986e5d3373e593dcaea33d8517b7da78cd
tizon9804 <javier.contreras@rappi.com>
2020-03-23 12:50:52 -0500

    add log for main

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from api.client import controller as client
 from infrastructure import config
+import infrastructure.log as log
+
+logger = log.get_logger("Main")
 
 app_name = "cartera"
 
@@ -15,4 +18,5 @@ client_controller = client.get_client_controller(configData)
 app.register_blueprint(client_controller, url_prefix=f'/api/{app_name}/client')
 
 if __name__ == "__main__":
+    logger.info("--------------- Init Cartera Api -------------")
     app.run()
